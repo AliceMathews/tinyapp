4a9a081b48e7b4e4c20638456d3865ffe9b17360
AliceMathews <alice_mathews@hotmail.co.uk>
2019-12-13 04:03:14 +0000

    updated data structure to deal with visits

--- database.js
+++ database.js
@@ -3,9 +3,11 @@ const bcrypt = require('bcrypt');
 
 const urlDatabase = {
   "b2xVn2": { longURL: "http://www.lighthouselabs.ca",
-              userID: "aJ48lW" },
+              userID: "aJ48lW",
+              visits: [] },
   "9sm5xK": { longURL: "http://www.google.com",
-              userID: "aJ48lW" }
+              userID: "aJ48lW",
+              visits: [] }
 };
 
 const users = { 
